6a0ac080e8067cc952e2e498e4fdaffda2d90f32
Halis Uyanık <halisuyanik@outlook.com>
2023-06-28 13:51:11 +0300

    fetch listing

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,7 +33,9 @@ export default async function RootLayout({
           <RentModal></RentModal>
           <SignupModal></SignupModal>
         </ClientOnly>
-        {children}
+        <div className="pb-20 pt-28">
+          {children}
+        </div>
       </body>
     </html>
   )
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,39 @@
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
+import ClientOnly from '@/components/ClientOnly'
+import Container from '@/components/container/Container'
+import EmptyState from '@/components/EmptyState'
+import getListings from '@/utils/getListings'
+import ListingCard from '@/components/listings/ListingCard'
+import getCurrentUser from '@/utils/getCurrentUser'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+export default async function Home() {
+  const isEmpty=true;
+  const listings=await getListings();
+  const currentUser=await getCurrentUser();
+
+  
+  if(listings.length===0){
+    return(
+      <ClientOnly>
+        <EmptyState showReset></EmptyState>
+      </ClientOnly>
+    )
+  }
+
   return (
-    <main>
-      <div>airbnb</div>
-    </main>
+    <ClientOnly>
+      <Container>
+        <div className='pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8'>
+          {listings.map((listing:any)=>{
+            return(
+              <ListingCard key={listing.id} data={listing}></ListingCard>
+            )
+          })}
+        </div>
+      </Container>
+    </ClientOnly>
   )
 }
